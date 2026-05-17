656e0db65c0761319c3c923971b1a4da50a8b241
Xiaohui Xie <139044257+Humyxie@users.noreply.github.com>
2026-05-17 11:33:53 +0000

    revert cors

--- silencewang/server/src/index.ts
+++ silencewang/server/src/index.ts
@@ -30,13 +30,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // 中间件
-app.use(cors(
-  {origin: [
-    'http://localhost:5173',
-    'https://silence-wang.vercel.app',
-],
-  credentials: true}
-));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 // 请求日志
