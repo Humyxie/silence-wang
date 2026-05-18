873b5a419b883e52cedc75ff2a2f246d5318b62b
Xiaohui Xie <139044257+Humyxie@users.noreply.github.com>
2026-05-18 07:42:38 +0000

    commit

--- silencewang/frontend/src/App.tsx
+++ silencewang/frontend/src/App.tsx
@@ -5,12 +5,12 @@ import Quiz from './pages/Quiz';
 import ResultPage from './pages/ResultPage';
 import About from './pages/About';
 
-const getBasename = () => {
-  const pathname = window.location.pathname;
-  // 假设部署在 /sulong-test/ 下，提取第一段路径
-  const match = pathname.match(/^\/[^\/]+/);
-  return match ? match[0] : '/';
-};
+// const getBasename = () => {
+//   const pathname = window.location.pathname;
+//   // 假设部署在 /sulong-test/ 下，提取第一段路径
+//   const match = pathname.match(/^\/[^\/]+/);
+//   return match ? match[0] : '/';
+// };
 
 function App() {
   return (
